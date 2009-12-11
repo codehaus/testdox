f4dff611a471e439c74e082077f44b16c3b183a7
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2009-12-11 16:47:58 +0000

    More IDEA 9.0 settings

--- trunk/src/scala/org/codehaus/testdox/intellij/AbstractTestElement.scala
+++ trunk/src/scala/org/codehaus/testdox/intellij/AbstractTestElement.scala
@@ -5,17 +5,17 @@ import com.intellij.psi.PsiElement
 
 abstract class AbstractTestElement extends TestElement {
 
-    def psiElement: PsiElement = NullPsiElement.INSTANCE
+  def psiElement: PsiElement = NullPsiElement.INSTANCE
 
-    def jumpToPsiElement() = false
+  def jumpToPsiElement() = false
 
-    def updatePresentation(presentation: Presentation): Unit = presentation.setEnabled(false)
+  def updatePresentation(presentation: Presentation): Unit = presentation.setEnabled(false)
 
-    def rename(controller: TestDoxController) {}
+  def rename(controller: TestDoxController) {}
 
-    def delete(controller: TestDoxController) {}
+  def delete(controller: TestDoxController) {}
 
-    def compareTo(o: TestElement): Int = if (this == o) 0 else -1
+  def compareTo(o: TestElement): Int = if (this == o) 0 else -1
 
-    override def toString = displayString
+  override def toString = displayString
 }
--- trunk/src/scala/org/codehaus/testdox/intellij/TestElement.scala
+++ trunk/src/scala/org/codehaus/testdox/intellij/TestElement.scala
@@ -7,17 +7,17 @@ import com.intellij.psi.PsiElement
 
 trait TestElement extends Comparable[TestElement] {
 
-    def psiElement: PsiElement
+  def psiElement: PsiElement
 
-    def jumpToPsiElement(): Boolean
+  def jumpToPsiElement(): Boolean
 
-    def displayString: String
+  def displayString: String
 
-    def icon: Icon
+  def icon: Icon
 
-    def updatePresentation(presentation: Presentation): Unit
+  def updatePresentation(presentation: Presentation): Unit
 
-    def rename(controller: TestDoxController): Unit
+  def rename(controller: TestDoxController): Unit
 
-    def delete(controller: TestDoxController): Unit
+  def delete(controller: TestDoxController): Unit
 }
