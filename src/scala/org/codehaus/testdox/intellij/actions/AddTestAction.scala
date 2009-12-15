afc93f56c960cc620885af85867b884b01a6a566
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2009-12-15 10:15:03 +0000

    Ported AddTestAction to Scala

---
+++ src/scala/org/codehaus/testdox/intellij/actions/AddTestAction.scala
@@ -0,0 +1,16 @@
+package org.codehaus.testdox.intellij.actions
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import org.codehaus.testdox.intellij.IconHelper._
+
+class AddTestAction extends BaseAction("Add Test", "Adds a test to the current unit test class", getIcon(ADD_TEST_ICON)) {
+
+  def actionPerformed(event: AnActionEvent) {
+    actionEvents.getTestDoxController(event).addTest
+  }
+
+  override def update(event: AnActionEvent) {
+    val testDoxFile = actionEvents.getTestDoxController(event).getCurrentTestDoxFile
+    event.getPresentation.setEnabled(testDoxFile != null && testDoxFile.canNavigateToTestedClass)
+  }
+}
