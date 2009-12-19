7a24fd8ec548610002fa948a9bab2269c69ff224
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2009-12-19 19:09:47 +0000

    Ported PackageTableModel to Scala

---
+++ trunk/src/scala/org/codehaus/testdox/intellij/config/PackageTableModel.scala
@@ -0,0 +1,76 @@
+package org.codehaus.testdox.intellij.config
+
+import org.codehaus.testdox.intellij.IconHelper
+
+import javax.swing._
+import javax.swing.event.ListSelectionEvent
+import javax.swing.event.ListSelectionListener
+import javax.swing.event.TableModelEvent
+import javax.swing.event.TableModelListener
+import javax.swing.table.TableModel
+
+import java.util.List
+import scala.collection.JavaConversions
+import scala.collection.mutable.ArrayBuffer
+
+private[config] class PackageTableModel extends TableModel with ListSelectionListener {
+
+  private val mappings = new ArrayBuffer[String]()
+  private val listeners = new ArrayBuffer[TableModelListener]()
+
+  def setMappings(newMappings: List[String]) {
+    mappings.clear()
+    mappings.insertAll(0, JavaConversions.asBuffer(newMappings))
+  }
+
+  def getMappings: List[String] = JavaConversions.asList(mappings)
+
+  def getColumnClass(columnIndex: Int): Class[_] = if (columnIndex == 0) classOf[String] else classOf[Icon]
+
+  val getColumnCount = 2
+
+  def getColumnName(columnIndex: Int) = if (columnIndex == 0) "Package mappings" else ""
+
+  def getRowCount = mappings.size
+
+  def setValueAt(aValue: Object, rowIndex: Int, columnIndex: Int) {
+    if (columnIndex == 0) {
+      mappings.remove(rowIndex)
+      if (aValue != null && aValue.asInstanceOf[String].length() > 0) {
+        mappings.insert(rowIndex, aValue.asInstanceOf[String])
+      }
+      fireTableModelChanged()
+    }
+  }
+
+  def getValueAt(rowIndex: Int, columnIndex: Int): Object = {
+    if (columnIndex == 1) IconHelper.getIcon(IconHelper.REMOVE_ICON) else mappings(rowIndex)
+  }
+
+  def isCellEditable(rowIndex: Int, columnIndex: Int) = columnIndex == 0
+
+  def addTableModelListener(listener: TableModelListener) {
+    if (!listeners.contains(listener)) {
+      listeners += listener
+    }
+  }
+
+  def removeTableModelListener(listener: TableModelListener) {
+    listeners -= listener
+  }
+
+  def addMapping(text: String) {
+    if (!mappings.contains(text)) {
+      mappings += text
+      fireTableModelChanged()
+    }
+  }
+
+  private def fireTableModelChanged() {
+    for (val listener <- listeners) {
+      listener.tableChanged(new TableModelEvent(this))
+    }
+  }
+
+  def valueChanged(event: ListSelectionEvent) {}
+}
