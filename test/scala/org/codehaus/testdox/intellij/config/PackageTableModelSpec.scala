1dec77b4cb2f2a0f93106d7261090d728bf3776e
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2010-08-20 05:43:12 +0000

    Rewrote PackageTableModel's test using specs

---
+++ test/scala/org/codehaus/testdox/intellij/config/PackageTableModelSpec.scala
@@ -0,0 +1,77 @@
+package org.codehaus.testdox.intellij.config
+
+import javax.swing.Icon
+import javax.swing.event.{TableModelEvent, TableModelListener}
+
+import org.specs.Specification
+import org.codehaus.testdox.intellij.Icons
+
+object PackageTableModelSpec extends Specification {
+
+  val model = new PackageTableModel()
+
+  "PackageTableModel" should {
+
+    "define that the first column contain strings and the second column contain icons" in {
+      model.getColumnClass(0).getName() must be equalTo classOf[String].getName()
+      model.getColumnClass(1).getName() must be equalTo classOf[Icon].getName()
+    }
+
+    "allow only the mapping column to be editable" in {
+      model.isCellEditable(0, 0) must be equalTo true
+      model.isCellEditable(0, 1) must be equalTo false
+    }
+
+    "return blank for remove column name" in {
+      model.getColumnName(1) must be equalTo ""
+    }
+
+    "return remove as value for all rows in remove column" in {
+      model.getValueAt(0, 1) must be equalTo Icons.getIcon(Icons.REMOVE_ICON)
+    }
+
+    "update table when a mapping is added" in {
+      val listener = new MyTableModelListener()
+      model.addTableModelListener(listener)
+
+      model.getRowCount() must be equalTo 0
+
+      model.addMapping("foo")
+
+      model.getRowCount() must be equalTo 1
+      listener.tableChanged must be equalTo true
+    }
+
+    "not add a new mapping if the mapping already exists" in {
+      model.addMapping("foo")
+      model.getRowCount() must be equalTo 1
+
+      val listener = new MyTableModelListener()
+      model.addTableModelListener(listener)
+      model.addMapping("foo")
+
+      model.getRowCount() must be equalTo 1
+      listener.tableChanged must be equalTo false
+    }
+
+    "remove a row from the table if the new value is blank" in {
+      model.addMapping("foo")
+      model.getRowCount() must be equalTo 1
+
+      val listener = new MyTableModelListener()
+      model.addTableModelListener(listener)
+      model.setValueAt("", 0, 0)
+
+      model.getRowCount() must be equalTo 0
+      listener.tableChanged must be equalTo true
+    }
+  }
+
+  class MyTableModelListener extends TableModelListener {
+    var tableChanged: Boolean = _
+
+    def tableChanged(event: TableModelEvent) {
+      tableChanged = true
+    }
+  }
+}
