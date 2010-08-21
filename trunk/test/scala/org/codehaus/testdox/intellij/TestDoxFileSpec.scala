b86e1f9e8e0c9784702dc172aaeacef78bc588f7
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2010-08-21 12:07:53 +0000

    Rewrote TestDoxFile's test using specs

---
+++ trunk/test/scala/org/codehaus/testdox/intellij/TestDoxFileSpec.scala
@@ -0,0 +1,34 @@
+package org.codehaus.testdox.intellij
+
+import org.specs.Specification
+import org.codehaus.testdox.intellij.ui.TestDoxTableModel
+
+object TestDoxFileSpec extends Specification {
+
+  val testDoxFile = new TestDoxFile(null, null, null, null, TestMethod.EMPTY_ARRAY) {
+      def updateModel(model: TestDoxTableModel) { }
+  }
+
+  "By default, a TestDox file" can {
+    "not represent a test class" in {
+      testDoxFile.isTestedClass() must be equalTo false
+    }
+
+    "not be unit tested by default" in {
+      testDoxFile.canBeUnitTested() must be equalTo false
+    }
+
+    "not navigate to the test class" in {
+      testDoxFile.canNavigateToTestClass() must be equalTo false
+    }
+
+    "not navigate to the tested class" in {
+      testDoxFile.canNavigateToTestedClass() must be equalTo false
+    }
+
+    "be represented as text" in {
+      testDoxFile.toString() must be equalTo
+          (testDoxFile.getClass().getName() + " { file: null; className: null; testClass: null; testedClass: null }")
+    }
+  }
+}
