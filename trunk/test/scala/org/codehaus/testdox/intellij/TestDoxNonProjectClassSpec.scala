75f89a55dc9d38e33c98cd856c339c02d2f29635
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2010-08-21 12:32:38 +0000

    Rewrote TestDoxNonProjectClass's test using specs

---
+++ trunk/test/scala/org/codehaus/testdox/intellij/TestDoxNonProjectClassSpec.scala
@@ -0,0 +1,18 @@
+package org.codehaus.testdox.intellij
+
+import org.specs.Specification
+
+object TestDoxNonProjectClassSpec extends Specification {
+
+  "The test element of a Java class that is not in the current project" should {
+    val testElement = TestDoxNonProjectClass.TEST_ELEMENT
+
+    "have a corresponding textual representation" in {
+      testElement.displayString must be equalTo "<font color=\"gray\">Not in current project</font>"
+    }
+
+    "use the warning icon as its graphical representation" in {
+      testElement.icon must be equalTo Icons.getIcon(Icons.WARNING_ICON)
+    }
+  }
+}
