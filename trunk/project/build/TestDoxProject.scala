f62e58fd016f3f381e091cfa6425a4624fa8df5a
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2010-08-24 05:59:38 +0000

    Started porting the Ant build script to SBT

--- trunk/project/build/TestDoxProject.scala
+++ trunk/project/build/TestDoxProject.scala
@@ -4,6 +4,8 @@ import webbytest.HtmlTestsProject
 
 class TestDoxProject(info: ProjectInfo) extends DefaultProject(info) with HtmlTestsProject {
 
+  // mapping Maven's project structure to TestDox's
+
   override def outputPath = "build"
 
   override def mainJavaSourcePath = "src" / "java"
@@ -13,8 +15,24 @@ class TestDoxProject(info: ProjectInfo) extends DefaultProject(info) with HtmlTe
   override def testJavaSourcePath = "src" / "test"
   override def testScalaSourcePath = "test" / "scala"
 
+  // loading all JAR's into the unmanaged classpath
+
   val ideaHome = system[File]("idea.home")
   val ideaLib = Path.fromFile(ideaHome.value) / "lib"
 
   override def unmanagedClasspath = super.unmanagedClasspath +++ descendents(ideaLib, "*.jar") +++ ("lib" ** "*.jar")
+
+  override def compileOptions = ExplainTypes :: CompileOption("-target:jvm-1.5") :: super.compileOptions.toList
+
+  // porting all key Ant tasks to SBT
+
+  lazy val build = task { None } describedAs "Runs the build used for continuous integration" dependsOn(clean, createTestReports, checkCoverage, createArtifact)
+
+  lazy val createTestReports = task { None }
+  lazy val checkCoverage = task { None }
+  lazy val createArtifact = task { createArtifactAction; None } describedAs "Zips up the plugin for deployment into IntelliJ IDEA" dependsOn compile
+
+  def createArtifactAction {
+    log.info("zipping up plugin for distribution...")
+  }
 }
