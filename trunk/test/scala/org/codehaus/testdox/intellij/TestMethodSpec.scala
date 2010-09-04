2e4ed5af9b3492e30f47839fca58e7a90bd58994
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2010-09-04 13:21:37 +0000

    Rewrote TestMethod's tests using specs

---
+++ trunk/test/scala/org/codehaus/testdox/intellij/TestMethodSpec.scala
@@ -0,0 +1,65 @@
+package org.codehaus.testdox.intellij
+
+import junitx.framework.ComparableAssert
+import com.intellij.psi.PsiMethod
+import org.codehaus.testdox.intellij.actions.RenameTestAction
+import org.codehaus.testdox.intellij.config.Configuration
+import org.specs.SpecificationWithJUnit
+import org.specs.mock.JMocker
+
+object TestMethodSpec extends SpecificationWithJUnit with JMocker {
+
+  "TestMethod" should {
+    val editorApi = mock[EditorApi]
+    val psiMethod = mock[PsiMethod]
+
+    "use the editor API to jump to the corresponding method" in {
+      expect { one(editorApi).jumpToPsiElement(psiMethod) will returnValue(true) }
+
+      val testMethod = new TestMethod(psiMethod, editorApi, null)
+      testMethod.jumpToPsiElement() must be equalTo true
+    }
+
+    "return zero when compared to an object that is not a test method" in {
+      val testMethod = new TestMethod(null, null, null)
+      val testInterface = new TestInterface(null, null, null, null)
+
+      testMethod.compareTo(testInterface) must be equalTo 0
+    }
+
+    "use its display string to define its natural order for comparison" in {
+      val methodName1 = "someMethod"
+      val methodName2 = "someOtherMethod"
+      val psiMethod2 = mock[PsiMethod]
+
+      expect {
+        atLeast(1).of(psiMethod).getName()  will returnValue(methodName1)
+        atLeast(1).of(psiMethod2).getName() will returnValue(methodName2)
+      }
+
+      val sentenceTranslator = new SentenceTranslator(new Configuration)
+      val testMethod1 = new TestMethod(psiMethod,  editorApi, sentenceTranslator)
+      val testMethod2 = new TestMethod(psiMethod2, editorApi, sentenceTranslator)
+
+      ComparableAssert.assertGreater(methodName2, testMethod1, testMethod2)
+    }
+
+    "use its display string as its textual representation" in {
+      val sentenceTranslator = new SentenceTranslator(new Configuration())
+      val testMethod = new TestMethod(psiMethod, editorApi, sentenceTranslator)
+
+      expect { atLeast(1).of(psiMethod).getName() will returnValue("someMethod") }
+
+      testMethod.toString() must be eq(testMethod.displayString)
+    }
+
+    "always enable the representation of an action when asked to update it" in {
+      val presentation = new RenameTestAction().getTemplatePresentation()
+      presentation.setEnabled(false)
+
+      new TestMethod(null, null, null).update(presentation)
+
+      presentation.isEnabled() must be equalTo true
+    }
+  }
+}
