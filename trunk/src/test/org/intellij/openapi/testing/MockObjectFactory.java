c677fdcf3c2e1075e53f30047947efaff77d78bd
fraz <fraz@37f2b646-1035-0410-b3b6-ebcf0f46b917>
2010-10-03 00:18:28 +0000

    Removed unused mock builders

--- trunk/src/test/org/intellij/openapi/testing/MockObjectFactory.java
+++ trunk/src/test/org/intellij/openapi/testing/MockObjectFactory.java
@@ -1,8 +1,5 @@
 package org.intellij.openapi.testing;
 
-import com.intellij.psi.JavaDirectoryServiceMockBuilder;
-import com.intellij.psi.PsiDirectoryMockBuilder;
-import com.intellij.psi.PsiPackageMockBuilder;
 import org.codehaus.testdox.intellij.EditorApiMockBuilder;
 import org.jmock.cglib.MockObjectTestCase;
 
@@ -16,17 +13,5 @@ public class MockObjectFactory {
 
     public EditorApiMockBuilder editorApi() {
         return new EditorApiMockBuilder(testCase);
-    }
-
-    public PsiDirectoryMockBuilder psiDirectory() {
-        return new PsiDirectoryMockBuilder(testCase);
-    }
-
-    public PsiPackageMockBuilder psiPackage() {
-        return new PsiPackageMockBuilder(testCase);
-    }
-
-    public JavaDirectoryServiceMockBuilder javaDirectoryService() {
-        return new JavaDirectoryServiceMockBuilder(testCase);
     }
 }
